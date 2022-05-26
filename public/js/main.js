7bbb7da9b5e776d99c8e036f0f7be02d081a5721
AnthoineSG <anthoine93@outlook.fr>
2022-05-26 18:06:35 +0200

    le front de base c'est good

--- public/js/main.js
+++ public/js/main.js
@@ -7,12 +7,14 @@ const app = {
         app.makeInDOM();
     },
 
-
     makeInDOM() {
         app.fetchLevel(2);
         app.fetchTag(3);
+        app.fetchQuiz(5);
+        app.fetchQuestion(4);
+        app.fetchAnswer(2);
+        app.fetchUser(1);
     },
-
 
     async fetchLevel(id) {
         const response = await fetch(`${app.url}/level/${id}`, { method: "GET" });
@@ -27,7 +29,6 @@ const app = {
         }
     },
 
-
     async fetchTag(id) {
         const response = await fetch(`${app.url}/tags/${id}`, { method: "GET" });
         if (response.ok) {
@@ -39,9 +40,59 @@ const app = {
         else {
             console.log(response);
         }
-    }
+    },
 
+    async fetchQuiz(id) {
+        const response = await fetch(`${app.url}/quiz/${id}`, { method: "GET" });
+        if (response.ok) {
+            const quiz = await response.json();
 
+            const quizInHTML = document.querySelector(".header__conteneur-quiz-title");
+            quizInHTML.textContent = quiz.title;
+        }
+        else {
+            console.log(response);
+        }
+    },
+
+    async fetchQuestion(id) {
+        const response = await fetch(`${app.url}/question/${id}`, { method: "GET" });
+        if (response.ok) {
+            const question = await response.json();
+
+            const questionInHTML = document.querySelector(".main__conteneur-question-title");
+            questionInHTML.textContent = question.question;
+        }
+        else {
+            console.log(response);
+        }
+    },
+
+    async fetchAnswer(id) {
+        const response = await fetch(`${app.url}/answer/${id}`, { method: "GET" });
+        if (response.ok) {
+            const answer = await response.json();
+
+            const answerInHTML = document.querySelector(".main__conteneur-response-title");
+            answerInHTML.textContent = answer.description;
+        }
+        else {
+            console.log(response);
+        }
+    },
+
+    async fetchUser(id) {
+        const response = await fetch(`${app.url}/user/${id}`, { method: "GET" });
+        if (response.ok) {
+            const user = await response.json();
+
+            const userInHTML = document.querySelector(".header__conteneur-user-name");
+            userInHTML.textContent = user.firstname;
+        }
+        else {
+            console.log(response);
+        }
+    },
 };
 
 document.addEventListener("DOMContentLoaded", app.init);
