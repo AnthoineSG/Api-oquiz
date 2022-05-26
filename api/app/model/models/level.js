42161a8fb7c39d77054cd8eb831e710949765c69
AnthoineSG <anthoine93@outlook.fr>
2022-05-26 10:41:43 +0200

    fin de jsdoc pour les level

--- api/app/model/models/level.js
+++ api/app/model/models/level.js
@@ -16,40 +16,72 @@ async function getAllLevel() {
     }
 }
 
+/**
+ * Recupere un level en fonction d'un id
+ * @returns Retourne une erreur
+ */
 async function getOneLevel(id) {
-    const query = {
-        text: `SELECT * FROM "level" WHERE "id" = $1 LIMIT 1;`,
-        values: [id]
-    };
-    const result = await dbconnect.query(query);
-    return result.rows[0];
+    try {
+        const query = {
+            text: `SELECT * FROM "level" WHERE "id" = $1 LIMIT 1;`,
+            values: [id]
+        };
+        const result = await dbconnect.query(query);
+        return result.rows[0];
+    } catch (error) {
+        return error;
+    }
 }
 
+/**
+ * Cree un level en BDD
+ * @returns Retourne une erreur
+ */
 async function createLevel(name) {
-    const query = {
-        text: `INSERT INTO "level" ("name") VALUES ($1) RETURNING "id", "name", "created_at", "updated_at";`,
-        values: [name]
-    };
-    const result = await dbconnect.query(query);
-    return result.rows[0];
+    try {
+        const query = {
+            text: `INSERT INTO "level" ("name") VALUES ($1) RETURNING "id", "name", "created_at", "updated_at";`,
+            values: [name]
+        };
+        const result = await dbconnect.query(query);
+        return result.rows[0];
+    } catch (error) {
+        return error;
+    }
 }
 
+/**
+ * Modifie un level en fonction de sont id
+ * @returns Retourne une erreur
+ */
 async function updateLevel(name, id) {
-    const query = {
-        text: `UPDATE "level" SET "name" = $1 WHERE "id" = $2 RETURNING "id", "name", "created_at", "updated_at";`,
-        values: [name, id]
-    };
-    const result = await dbconnect.query(query);
-    return result.rows[0];
+    try {
+        const query = {
+            text: `UPDATE "level" SET "name" = $1 WHERE "id" = $2 RETURNING "id", "name", "created_at", "updated_at";`,
+            values: [name, id]
+        };
+        const result = await dbconnect.query(query);
+        return result.rows[0];
+    } catch (error) {
+        return error;
+    }
 }
 
+/**
+ * Supprime un level en BDD
+ * @returns Retourne une erreur
+ */
 async function deleteLevel(id) {
-    const query = {
-        text: `DELETE FROM "level" WHERE "id" = $1 RETURNING "id", "name", "created_at", "updated_at";`,
-        values: [id]
-    };
-    const result = await dbconnect.query(query);
-    return result.rows[0];
+    try {
+        const query = {
+            text: `DELETE FROM "level" WHERE "id" = $1 RETURNING "id", "name", "created_at", "updated_at";`,
+            values: [id]
+        };
+        const result = await dbconnect.query(query);
+        return result.rows[0];
+    } catch (error) {
+        return error;
+    }
 }
 
 module.exports = {
